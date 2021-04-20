04de23dfef0c45ad906448f900fd662b3ebb49cc
Peter Edwards <pete@bjorsq.net>
2021-04-20 10:22:31 +0100

    Almost completed form scripts and styles - still to do submission and processing

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,102 +1,148 @@
 (function($){
-  $('#is_gift').on('click', function(){
-    if ($(this).is(':checked')) {
-      $('#giftname_field,#giftaddress_field').show().attr('required',true);
-    } else {
-      $('#giftname_field,#giftaddress_field').hide().removeAttr('required');
-      $('#giftname,#giftaddress').val('');
-    }
-  });
-  $('#payment_method').on('change',function(){
-    $('.payment_method_text').hide();
-    if ($('#payment_method').val() !== '') {
-      $(this).removeClass('error');
-      $('#payment_method_'+$('#payment_method').val()).show();
-    }
-  });
-  $('#membership_category').on('change', function(){
-    $('#jointname_field,#groupname_field').hide().removeAttr('required');
-    var mcat = $('#membership_category').val();
-    if (mcat !== '') {
-      $('#'+mcat+'name_field').show().attr('required',true);
-    }
-  });
-  var checkForm = function(){
-    // Clear any previous errors
-    $('span.errormsg').remove();
-    $('input textarea select').removeClass('error');
-    // Check name
-    if ( $.trim($('#fullname').val()) === '' ){
-      $('#fullname').addClass('error').after('<span class="errormsg">Please enter your full name.</span>');
-    }
-    // Check email
-    var emailRE = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
-    if (!$.trim($('#emailaddress').val()).match(emailRE)){
-      $('#emailaddress').val('');
-    }
-    if ( $.trim($('#emailaddress').val()) === '' ){
-      $('#emailaddress').addClass('error').after('<span class="errormsg">Please enter your email address</span>');
-    }
-    // Check address
-    if ( $.trim($('#address').val()) === '' ){
-      $('#address').addClass('error').after('<span class="errormsg">Please enter your address and postcode.</span>');
-    }
-    // If this is a gift, check recipient details
-    if ($('#is_gift').is(':checked')){
-      // Check recipient name
-      if ( $.trim($('#giftname').val()) === '' ){
-        $('#giftname').addClass('error').after('<span class="errormsg">Please enter the full name of the recipient.</span>');
-      }
-      // Check recipient address
-      if ( $.trim($('#giftaddress').val()) === '' ){
-        $('#giftaddress').addClass('error').after('<span class="errormsg">Please enter the address and postcode of the recipient.</span>');
-      }      
-    }
-    if ( $('#membership_category').val() === '' ){
-      $('#membership_category').addClass('error').after('<span class="errormsg">Please select a Membership type.</span>');
-    }
-    if ( $('#payment_method').val() === '' ){
-      $('#payment_method').addClass('error').after('<span class="errormsg">Please select a Payment method.</span>');
-    }
-    if (!$('.error').length) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  $('input,textarea,select').on('focus', function(){
-    if ( $(this).hasClass('error') ) {
-      $(this).removeClass('error');
-      $(this).next().remove();
-    }
-  });
-  $('#apply').on('click',function(e){
-    if ( ! checkForm() ) {
-      e.preventDefault();
-    } else {
-      var data = {
-        'fullname': $('#fullname').val(),
-        'emailaddress': $('#emailaddress').val(),
-        'address': $('#address').val(),
-        'telephone': $('#telephone').val(),
-        'is_gift': ($('#is_gift').is(':checked')?'yes':'no'),
-        'gift_recipient_name': $('#giftname').val(),
-        'gift_recipient_address': $('#giftaddress').val(),
-        'membership_type': $('input[name="membership_type"]:checked').val(),
-        'membership_category': $('#membership_category').val(),
-        'joint_member_name': $('#jointname').val(),
-        'group_member_name': $('#groupname').val(),
-        'payment_method': $('#payment_method').val()
-      };
-      var overlay_text = '<h2>Form checked and ready for submission</h2><table><thead><tr><th>Field name</th><th>Field value</th></tr></thead><tbody>';
-      for(f in data){
-        overlay_text += '<tr><td>'+f+'</td><td>'+data[f]+'</td></tr>';
-      }
-      overlay_text += '</tbody></table>';
-      $('body').append('<div class="overlay"><div class="text">'+overlay_text+'</div></div>');
-      $(document).on('click', '.overlay', function(){
-        $(this).remove();
-      });
-    }
-  });
+	$('#is_gift').on('click', function(){
+		if ($(this).is(':checked')) {
+			$('#gift_recipient_fields').removeClass('hidden');
+			$('#giftname,#giftaddress').attr('required',true).removeClass('error').next('.error_text').remove();
+			$('#gift_aid_disabled').removeClass('hidden');
+			$('#gift_aid').attr('disabled',true);
+		} else {
+			$('#gift_recipient_fields').addClass('hidden');
+			$('#giftname,#giftaddress').attr('required',null).val('');
+			$('#gift_aid_disabled').addClass('hidden');
+			$('#gift_aid').attr('disabled',null);
+		}
+	});
+	$('#gift_aid').on('click', function(){
+		if ($(this).is(':checked')) {
+			$('#gift_aid_fields').removeClass('hidden');
+			$('#gift_recipient_fields').addClass('hidden');
+			$('#is_gift_disabled').removeClass('hidden');
+			$('#is_gift').attr('disabled',true);
+		} else {
+			$('#gift_aid_fields').addClass('hidden');
+			$('#is_gift_disabled').addClass('hidden');
+			$('#is_gift').attr('disabled',null);
+		}
+	});
+	$('#payment_method').on('change',function(){
+		$('.payment_method_text').hide();
+		if ($('#payment_method').val() !== '') {
+			$(this).removeClass('error');
+			$('#payment_method_'+$('#payment_method').val()).show();
+		}
+	});
+	$('#membership_category').on('change', function(){
+		$('#jointname_field,#groupname_field').hide().attr('required',null);
+		var mcat = $('#membership_category').val();
+		if (mcat !== '') {
+			$('#'+mcat+'name_field').show().attr('required',true);
+		}
+	});
+	var checkForm = function(){
+		// Clear any previous errors
+		$('span.error_text').remove();
+		$('input textarea select').removeClass('error');
+		// Check name
+		if ( $.trim($('#first_name').val()) === '' ){
+			$('#first_name').addClass('error').after('<span class="error_text">Please enter your first name.</span>');
+		}
+		if ( $.trim($('#last_name').val()) === '' ){
+			$('#last_name').addClass('error').after('<span class="error_text">Please enter your last name.</span>');
+		}
+		// Check email
+		var emailRE = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+		if (!$.trim($('#emailaddress').val()).match(emailRE)){
+			$('#emailaddress').val('');
+		}
+		if ( $.trim($('#emailaddress').val()) === '' ){
+			$('#emailaddress').addClass('error').after('<span class="error_text">Please enter your email address</span>');
+		}
+		// Check address
+		if ( $.trim($('#address').val()) === '' ){
+			$('#address').addClass('error').after('<span class="error_text">Please enter your address.</span>');
+		}
+		if ( $.trim($('#postcode').val()) === '' ){
+			$('#postcode').addClass('error').after('<span class="error_text">Please enter your postcode.</span>');
+		}
+		if ($('#is_gift').is(':checked') && $('#gift_aid').is(':checked')){
+			// Can't do both - uncheck
+			$('#is_gift').prop('checked', false);
+			$('#gift_aid').prop('checked', false);
+			$('#gift_aid_confirm').prop('checked', false);
+			$('#is_gift_disabled').addClass('hidden');
+			$('#gift_aid_disabled').addClass('hidden');
+			$('#gift_aid_fields').addClass('hidden');
+			$('#gift_recipient_fields').addClass('hidden');
+			$('#giftname,#giftaddress').attr('required',null).val('');
+		}
+		// If this is a gift, check recipient details
+		if ($('#is_gift').is(':checked')){
+			// Check recipient name
+			if ( $.trim($('#giftname').val()) === '' ){
+				$('#giftname').addClass('error').after('<span class="error_text">Please enter the full name of the recipient.</span>');
+			}
+			// Check recipient address
+			if ( $.trim($('#giftaddress').val()) === '' ){
+				$('#giftaddress').addClass('error').after('<span class="error_text">Please enter the address and postcode of the recipient.</span>');
+			}
+		}
+		// If Gift Aid is being used, check confirmation
+		if ($('#gift_aid').is(':checked')){
+			// make sure confirmation is checked
+			if(!$('#gift_aid_confirm').is(':checked')){
+				$('#gift_aid_confirm').addClass('error');
+				$('#gift_aid_fields').append('<span class="error_text indent">Please confirm you agree to the Gift Aid declaration.</span>');
+			}
+		}
+		if ( $('#membership_category').val() === '' ){
+			$('#membership_category').addClass('error').after('<span class="error_text">Please select a Membership type.</span>');
+		}
+		if ( $('#payment_method').val() === '' ){
+			$('#payment_method').addClass('error').after('<span class="error_text">Please select a Payment method.</span>');
+		}
+		if (!$('.error').length) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+	$('input[type=text],input[type=email],textarea,select').on('focus', function(){
+		if ( $(this).hasClass('error') ) {
+			$(this).removeClass('error');
+			$(this).next('.error_text').remove();
+		}
+	});
+	$('#gift_aid_confirm').on('focus',function(){
+		$('#gift_aid_confirm').removeClass('error');
+		$('#gift_aid_fields').find('.error_text').remove();
+	});
+	$('#apply').on('click',function(e){
+		if ( ! checkForm() ) {
+			e.preventDefault();
+		} else {
+			var data = {
+				'fullname': $('#fullname').val(),
+				'emailaddress': $('#emailaddress').val(),
+				'address': $('#address').val(),
+				'telephone': $('#telephone').val(),
+				'is_gift': ($('#is_gift').is(':checked')?'yes':'no'),
+				'gift_recipient_name': $('#giftname').val(),
+				'gift_recipient_address': $('#giftaddress').val(),
+				'membership_type': $('input[name="membership_type"]:checked').val(),
+				'membership_category': $('#membership_category').val(),
+				'joint_member_name': $('#jointname').val(),
+				'group_member_name': $('#groupname').val(),
+				'payment_method': $('#payment_method').val()
+			};
+			var overlay_text = '<h2>Form checked and ready for submission</h2><table><thead><tr><th>Field name</th><th>Field value</th></tr></thead><tbody>';
+			for(f in data){
+				overlay_text += '<tr><td>'+f+'</td><td>'+data[f]+'</td></tr>';
+			}
+			overlay_text += '</tbody></table>';
+			$('body').append('<div class="overlay"><div class="text">'+overlay_text+'</div></div>');
+			$(document).on('click', '.overlay', function(){
+				$(this).remove();
+			});
+		}
+	});
 })(jQuery);
--- index.md
+++ index.md
@@ -1,34 +1,4 @@
-<div class="application_form">
-<p><label for="fullname" class="required">Title, first name and surname</label><input type="text" name="fullname" id="fullname" required></p>
-<p><label for="emailaddress" class="required">Email address</label><input type="email" name="emailaddress" id="emailaddress" required></p>
-<p><label for="address" class="required">Address including postcode</label><textarea name="address" id="address" required></textarea></p>
-<p><label for="telephone">Telephone number</label><input type="tel" name="telephone" id="telephone" /></p>
-  <p><label for="is_gift"><input type="checkbox" name="is_gift" id="is_gift" />Check this box if this is a gift</label></p>
-<p class="hidden" id="giftname_field"><label for="giftname" class="required">Recipient title, first name and surname</label><input type="text" name="giftname" id="giftname"></p>
-<p class="hidden" id="giftaddress_field"><label for="giftaddress" class="required">Recipient address including postcode</label><textarea name="giftaddress" id="giftaddress"></textarea></p>
-  <p><span class="label" class="required">Membership type</span><label for="membership_type_new"><input type="radio" name="membership_type" id="membership_type_new" value="new" checked>New</label>
-  <label for="membership_type_renewal"><input type="radio" name="membership_type" id="membership_type_renewal" value="renewal">Renewal</label>
-</p>
-<p><label for="membership_category" class="required">Membership Category</label><select name="membership_category" id="membership_category">
-  <option value="">Select your membership category...</option>
-  <option value="individual">Individual (£25)</option>
-  <option value="joint">Joint (two Friends at the same address) £35</option>
-  <option value="group">Group / Business (£40)</option>
-  </select>
-</p>
-<p class="hidden" id="jointname_field"><label for="jointname" class="required">Title, first name and surname of joint member</label><input type="text" name="jointname" id="jointname" /></p>
-<p class="hidden" id="groupname_field"><label for="groupname" class="required">Group / Business Name</label><input type="text" name="groupname" id="groupname" /></p>
-<p><label for="payment_method" class="required">Payment method</label><select name="payment_method" id="payment_method" />
-  <option value="">Select your preferred payment method...</option>
-  <option value="paypal">PayPal</option>
-  <option value="transfer">Bank Transfer</option>
-  <option value="direct">Direct Debit</option>
-  <option value="cheque">Cheque</option>
-</select>
-</p>
-<p class="hidden payment_method_text" id="payment_method_paypal">When you click on the submit button, you will be redirected to PayPal to complete payment</p>
-<p class="hidden payment_method_text" id="payment_method_transfer">Our banking details will be included in your acknowledgement email.</p>
-<p class="hidden payment_method_text" id="payment_method_direct">We will send you details of how to set up a Direct Debit payment in your acknowledgement email.</p>
-<p class="hidden payment_method_text" id="payment_method_cheque">We will send you the address to post cheques to  in your acknowledgement email.</p>
-<p><button type="submit" name="apply" id="apply">Apply for membership</button></p>
-</div>
+---
+layout: application_form
+---
+
